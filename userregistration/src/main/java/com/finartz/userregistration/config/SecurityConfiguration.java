c05a4f69565c311377ab5b1cc2a2501b1de47e64
ecettt <ece.tipici.et@gmail.com>
2024-08-01 09:45:40 +0300

    fixed CORS configuration

--- userregistration/src/main/java/com/finartz/userregistration/config/SecurityConfiguration.java
+++ userregistration/src/main/java/com/finartz/userregistration/config/SecurityConfiguration.java
@@ -53,7 +53,6 @@ public class SecurityConfiguration {
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
